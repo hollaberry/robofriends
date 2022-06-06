e6b30929ad1b4e7fd992eb7e9d6dd72535215a47
hollaberry <69259629+hollaberry@users.noreply.github.com>
2022-06-06 04:53:49 -0700

    first commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'sincere@april.biz'
+},
+{
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+},
+{
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+},
+{
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@Kory.org'
+},
+{
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kanren',
+    email: 'Lucio_Heettinger@annie.ca'
+},
+{
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dac@jasper.info'
+},
+{
+    id: 7,
+    name: 'Matt Powel',
+    username: 'pwelm',
+    email: 'longman@manu.net'
+},
+{
+    id: 8,
+    name: 'Emily foxgrove',
+    username: 'flems',
+    email: 'rapido12@inscrire.net'
+},
+{
+    id: 9,
+    name: 'sean James',
+    username: 'touchy',
+    email: 'klomet@eng.fc'
+},
+{
+    id: 10,
+    name: 'Frank Junior',
+    username: 'kayjay',
+    email: 'lens@wesley.com'
+}
+];
